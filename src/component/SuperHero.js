f94f51e17e9055384678223ee5a63eba2c197d36
mahfuz-antor <antorcnj2014@gmail.com>
2023-01-03 11:47:45 +0600

    super hero data showing

--- src/component/SuperHero.js
+++ src/component/SuperHero.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const SuperHero = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+  // fetching data from localhost:4000
+  useEffect(() => {
+    axios.get("http://localhost:4000/superheros").then((res) => {
+      setData(res.data);
+      setIsLoading(false);
+    });
+  }, []);
+  console.log(data);
+
   return (
     <>
       <h2 className="fs-1">Super Heros Page</h2>
+      {data.map((hero) => (
+        <h4 key={hero.id} className="fs-4">
+          {hero.name}
+        </h4>
+      ))}
     </>
   );
 };
